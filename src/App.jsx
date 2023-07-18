08f90de1e4b84d4d943b94d230c0df2a4385797b
fredkamm <fred.kamm95@gmail.com>
2023-07-18 13:39:19 -0400

    dayjs installed confirmed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import dayjs from 'dayjs'
+
+//import dayjs from 'dayjs' // ES 2015
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const now = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  console.log(now);
 
   return (
     <>
@@ -16,7 +23,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{now}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
