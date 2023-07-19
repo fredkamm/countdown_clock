d575b9663818d85ea9a7b8e9f41bbd5b96b3d851
fredkamm <fred.kamm95@gmail.com>
2023-07-19 19:48:26 -0400

    countdown logic has been implimented; need some styling and determine with countdown clocks to have

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import dayjs from 'dayjs'
-
-//import dayjs from 'dayjs' // ES 2015
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
+  const [futureDate, setFutureDate] = useState("");
+  const [timeDifference, setTimeDifference] = useState({});
 
-  const now = dayjs().format('YYYY-MM-DD HH:mm:ss')
-  console.log(now);
+  useEffect(() => {
+    // Update date and time every second
+    const interval = setInterval(() => {
+      setCurrentDate(dayjs().format("dddd" + ", " + "MMMM D YYYY"));
+      setCurrentTime(dayjs().format("hh:mm:ss A"));
+    }, 1000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    // Update time difference every 100 milliseconds
+    const interval = setInterval(() => {
+      if (futureDate) {
+        const futureDateTime = dayjs(futureDate);
+        const now = dayjs();
+
+        const diffInMilliseconds = futureDateTime.diff(now);
+        const diffInUnits = calculateTimeDifference(diffInMilliseconds);
+
+        setTimeDifference(diffInUnits);
+      }
+    }, 100);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, [futureDate]);
+
+  const handleFutureDateChange = (event) => {
+    setFutureDate(event.target.value);
+  };
+
+  const calculateTimeDifference = (difference) => {
+    return {
+      milliseconds: difference,
+      seconds: dayjs(difference).diff(0, "seconds"),
+      minutes: dayjs(difference).diff(0, "minutes"),
+      hours: dayjs(difference).diff(0, "hours"),
+      days: dayjs(difference).diff(0, "days"),
+      weeks: dayjs(difference).diff(0, "weeks"),
+      months: dayjs(difference).diff(0, "months"),
+      years: dayjs(difference).diff(0, "years"),
+    };
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{now}</h1>
+      <h1>{currentDate}</h1>
+      <h1>{currentTime}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <input
+          type="datetime-local"
+          value={futureDate}
+          onChange={handleFutureDateChange}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>Time Difference:</h2>
+        {futureDate ? (
+          <ul>
+            <li>Milliseconds: {timeDifference.milliseconds}</li>
+            <li>Seconds: {timeDifference.seconds}</li>
+            <li>Minutes: {timeDifference.minutes}</li>
+            <li>Hours: {timeDifference.hours}</li>
+            <li>Days: {timeDifference.days}</li>
+            <li>Weeks: {timeDifference.weeks}</li>
+            <li>Months: {timeDifference.months}</li>
+            <li>Years: {timeDifference.years}</li>
+          </ul>
+        ) : (
+          <p>Enter a future date above to calculate the difference.</p>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
