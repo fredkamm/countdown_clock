0e266b354b6ff4512df10af0b9023689f14288bb
fredkamm <fred.kamm95@gmail.com>
2023-08-26 22:07:53 -0400

    deployment test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
-import DarkMode from "./components/helpers/darkMode";
+import DarkMode from "./components/helpers/DarkMode";
 import Clock from "./components/clock";
 import Body from "./components/body";
 
